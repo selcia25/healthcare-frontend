eea334901bd7b00265d3cd2638fe8940e007fcdd
SELCIA S <107830564+selcia25@users.noreply.github.com>
2023-03-27 23:02:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function bookAppointment() {
+  var myText = "Appointment booked tomorrow, take care!";
+  alert(myText);
+}
+
+function donateUs() {
+  let charity = prompt("How much would you like to donate?");
+  if (charity.length > 0) {
+    alert("Thank you for supporting us!🥰");
+  } else {
+    alert(
+      "Your generosity ensures that our doors has access to the best treatment and care possible.!🙏"
+    );
+  }
+}
+
+function donate55() {
+  alert(" Thank you for celebrating a birth - God bless!🙏❤");
+}
+
+function donate100() {
+  alert(
+    "Thanks for your great help! Your donations help us to deliver programs and medical equipment, and drive innovation and research across all areas of the hospital.❤🙏"
+  );
+}
+
+function donate250() {
+  alert(
+    "Thank you for supporting women and families during their stay – every donation makes an impact.❤🙏"
+  );
+}
+
+function submitButton() {
+  alert("We'll get back to you soon, take care until then!❤");
+}
